b931f30e74a3ba41e6f4cb63177b655126af957c
anytimemusic <bima220711@gmail.com>
2026-04-07 17:30:52 +0700

    nampilin perinth

--- Array.cpp
+++ Array.cpp
@@ -20,13 +20,20 @@ int main ()
     cin >> datamu[3];
     cout << "Isikan data kelima : ";
     cin >> datamu[4];
+    cout << endl;
+
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
 
     for (int i = 0; i <= 4; i++)
     {
         cout << "Isikan data ke-" << (i + 1) << " : ";
         cin >> datamu[i];
     }
-    
+
     for (int i = 0; i <= 4; i++)
     {
         cout << "Data ke-" << (i + 1) << " : " << datamu[i] << endl;
